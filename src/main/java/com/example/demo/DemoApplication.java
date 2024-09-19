785ab9e9eca0b28d62849b9b66d9376c21ef3a9d
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2024-09-19 21:24:50 +0530

    my first commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-		System.out.println("HEllo World");
-		
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+    System.out.println("HEllo World");
+    // this some test code.
+    System.out.println("Hello Anurag");
+
+  }
 
 }
